621e3df0ceea8987be97493e7a2f850fa874b77b
ErikSLee <ErikSLee@gmail.com>
2021-10-04 17:04:08 -0500

    tank reomval

--- src/main/java/frc/robot/commands/TankDrive.java
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -36,7 +36,7 @@ public class TankDrive extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_drive.driveTank(m_left.getAsDouble(), m_right.getAsDouble());
+    m_drive.drive(m_left.getAsDouble(), m_right.getAsDouble());
   }
 
   // Called once the command ends or is interrupted.
